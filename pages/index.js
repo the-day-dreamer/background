05f75fbd10a2b2d0af75ee04b8bbc176d9945097
the-day-dreamer <103681398+the-day-dreamer@users.noreply.github.com>
2023-03-23 18:03:55 +0530

    b

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-
+import Navbar from '@/components/Navbar'
+import Form from '@/components/Form'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -15,7 +16,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        
+      <Navbar/>
+      <Form/>
       </main>
     </>
   )
